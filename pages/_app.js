f25d25568d8abc25e5f959814b997318fc09f438
Leandro Musso <leandro@trama.solutions>
2021-01-12 21:54:15 -0300

    pwa

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
 import '../styles/globals.scss'
 import 'react-toastify/dist/ReactToastify.css';
 
-
 function MyApp({ Component, pageProps }) {
+
+  React.useEffect(() => {
+    if("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+       navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, [])
+
   return (
     <Component {...pageProps} />
   )
